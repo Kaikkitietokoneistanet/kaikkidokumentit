69709f5e0c55e42d142a3ea7d3ace98cf21eeffb
Kaikkitietokoneista <info@kaikkitietokoneista.net>
2021-09-22 14:28:09 +0300

    Basic structure for SimpleDocument

--- SimpleDocument-backend/app.js
+++ SimpleDocument-backend/app.js
@@ -1,9 +1,36 @@
-const Hapi = require('@hapi/hapi');
+'use strict';
 
-const server = Hapi.server({
-    load: { 
-        sampleInterval: 1000 
-    },
-    port: 8080
+const Hapi = require('@hapi/hapi');
+const { newDocument } = require('./document')
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: '0.0.0.0'
+    });
+
+    // TODO: switch to routes folder
+
+    /**
+     * Create new document
+     */
+    server.route({
+        method: 'POST',
+        path: '/',
+        handler: (request, h) => {
+            newDocument()
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    // process.exit(1);
 });
 
+init();
---
+++ SimpleDocument-backend/document/newDocument.js
@@ -0,0 +1,9 @@
+/**
+ * @name - document name
+ * @content - document content
+ * @owner - document owner
+ * @collection - mongodb collection
+ */
+module.exports = newDocument = ({ name, content, owner, collection }) => {
+    client
+}
