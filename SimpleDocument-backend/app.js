2a6dab78baccfbf2b9b035a8c6d8fb0f0d5a3d4c
Kaikkitietokoneista <info@kaikkitietokoneista.net>
2021-09-22 14:04:14 +0300

    Added @hapi/hapi

--- SimpleDocument-backend/app.js
+++ SimpleDocument-backend/app.js
@@ -1,4 +1,9 @@
 const Hapi = require('@hapi/hapi');
 
-const server = Hapi.server({ load: { sampleInterval: 1000 } });
+const server = Hapi.server({
+    load: { 
+        sampleInterval: 1000 
+    },
+    port: 8080
+});
 
